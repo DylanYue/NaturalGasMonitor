742b64b23dcbe45eaa689c10199a17f42b2887c3
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-12 02:45:17 -0600

    A little experiment

--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -62,7 +62,7 @@ while 1:
 	
 	if not GPIO.input(U_pin):
 		draw.text((0,-2), "Up button", font = font, fill = 255)
-		draw.text((2,4), "Test", font = font, fill = 255)
+		draw.text((2,10), "Test", font = font, fill = 255)
 	else:
 		draw.text((0,-2), "Hehe", font = font, fill = 255)
 		
