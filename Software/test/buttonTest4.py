945e0c9ad60480116dfcd89af0bceac398f22ff6
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-12 16:10:51 -0600

    Added button class

--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -120,7 +120,7 @@ class PushButton(object):
             False  ==> button not pressed
         """
         pressed = GPIO.wait_for_edge(self.pin_number, GPIO.FALLING)
-        time.sleep(0.15)  # Wait 0.15s for the bouncing effect of physical switches
+        time.sleep(0.25)  # Wait 0.25s for the bouncing effect of physical switches
         return True if pressed else False
 
     def HoldTime(self):
