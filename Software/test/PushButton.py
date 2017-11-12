1b8d70324be4bd22a3192db69ae838c9f4d07f43
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-12 16:29:23 -0600

    Separated PushButton class into individual file

---
+++ Software/test/PushButton.py
@@ -0,0 +1,40 @@
+class PushButton(object):
+
+    def __init__(self, pin_number):
+        self.pin_number = pin_number
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin_number, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+    def ButtonDown(self):
+        """ This method detects if the button is down
+            True  ==> button down
+            False ==> button up
+        """
+        return True if not GPIO.input(self.pin_number) else False
+
+    def ButtonPressed(self):
+        """ This method detects if the button is pressed once
+            True   ==> button pressed once
+            False  ==> button not pressed
+        """
+        pressed = GPIO.wait_for_edge(self.pin_number, GPIO.FALLING)
+        time.sleep(0.45)  # Wait 0.25s for the bouncing effect of physical switches
+        return True if pressed else False
+
+    def HoldTime(self):
+        """ This method returns the time the button is held down
+        """
+        # Return 0 is button is not down.
+        if not self.ButtonDown():
+            return 0
+        # Return the time the button is held down.
+        else:
+            start = time.time()
+            while self.ButtonDown():
+                time.sleep(0.01)
+                # If the button has been holding down for more than 5 seconds
+                # break the loop and return the hold time.
+                if (time.time() - start) > 5:
+                    break
+            length = time.time() - start
+            return length
--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -9,8 +9,7 @@ import Adafruit_SSD1306
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
-# Input pins:
+from PushButton import PushButton
 
 # Input pins:
 L_pin = 27 
@@ -18,7 +17,6 @@ R_pin = 23
 C_pin = 4 
 U_pin = 17 
 D_pin = 22 
- 
 A_pin = 5 
 B_pin = 6 
 
@@ -90,47 +88,6 @@ def SelectorPosConditioner (rawPos):
 def PlaceSelector(rowNumber):
 	DrawText(0, SelectorPosConditioner(rowNumber), "->")
 
-
-class PushButton(object):
-
-    def __init__(self, pin_number):
-        self.pin_number = pin_number
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin_number, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-    def ButtonDown(self):
-        """ This method detects if the button is down
-            True  ==> button down
-            False ==> button up
-        """
-        return True if not GPIO.input(self.pin_number) else False
-
-    def ButtonPressed(self):
-        """ This method detects if the button is pressed once
-            True   ==> button pressed once
-            False  ==> button not pressed
-        """
-        pressed = GPIO.wait_for_edge(self.pin_number, GPIO.FALLING)
-        time.sleep(0.45)  # Wait 0.25s for the bouncing effect of physical switches
-        return True if pressed else False
-
-    def HoldTime(self):
-        """ This method returns the time the button is held down
-        """
-        # Return 0 is button is not down.
-        if not self.ButtonDown():
-            return 0
-        # Return the time the button is held down.
-        else:
-            start = time.time()
-            while self.ButtonDown():
-                time.sleep(0.01)
-                # If the button has been holding down for more than 5 seconds
-                # break the loop and return the hold time.
-                if (time.time() - start) > 5:
-                    break
-            length = time.time() - start
-            return length
 
 # Initialize Button Objects			
 ButtonL = PushButton(L_pin)
