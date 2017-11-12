719f3bd124e715486718b7c552ccd1f7fc7ea0b5
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-12 16:39:51 -0600

    Adjusted debounce

--- Software/test/PushButton.py
+++ Software/test/PushButton.py
@@ -20,7 +20,7 @@ class PushButton(object):
             True   ==> button pressed once
             False  ==> button not pressed
         """
-        pressed = GPIO.input(self.pin_number)
+        pressed = not GPIO.input(self.pin_number)
         time.sleep(0.25)  # Wait 0.25s for the bouncing effect of physical switches
         return True if pressed else False
 
