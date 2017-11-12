e4276df395e4d8d295ea61033f17a60c2e76a046
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-12 02:18:02 -0600

    better work you little shit lolll

--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -11,24 +11,48 @@ from PIL import ImageFont
 
 # Input pins:
 
-GPIO.setmode(GPIO.BCM)
-U_pin = 22
+# Input pins:
+L_pin = 27 
+R_pin = 23 
+C_pin = 4 
+U_pin = 17 
+D_pin = 22 
+ 
+A_pin = 5 
+B_pin = 6 
+ 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
 
-GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Raspberry Pi pin configuration:
+RST = None
+ 
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-disp = Adafruit_SSD1306.SSD1306_128_64(None)
-
+# Initialize library
 disp.begin()
 
+# Clear display
 disp.clear()
 disp.display()
 
+# Create blank image for drawing
+# Make sure to create image with mode '1' for 1-bit color
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
+# Get the drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
+# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline = 0, fill = 0)
 
 font = ImageFont.load_default()
@@ -36,3 +60,6 @@ font = ImageFont.load_default()
 while 1:
 	if not GPIO.input(U_pin):
 		draw.text((0,-2), "Up button", font = font, fill = 255)
+		
+	disp.image(image)
+	disp.display()
