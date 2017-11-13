d562cd79706801d407ec0cb491dccb6eb35ba3a9
fangjia li <hollandlinus@gmail.com>
2017-11-13 13:46:00 -0800

    add data sample and url /getData to fetch data from .csv file

--- raspberry-pi-server/routes/index.js
+++ raspberry-pi-server/routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var csv = require("fast-csv");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log("right here");
     res.render('index', {title: 'Express'});
 });
+router.get('/getData', async function (req, res, next) {
+    var dataSample = [];
+    console.log(__dirname + '/../../DataSample/Data.csv');
+    var input = fs.createReadStream(__dirname + '/../../DataSample/Data.csv');
+    var csvStream = csv()
+        .on("data", function (data) {
+            dataSample.push(data);
+        })
+        .on("end", function () {
+            console.log("done", dataSample);
+            res.json(dataSample);
+        });
+    input.pipe(csvStream);
+
+})
 
 module.exports = router;
