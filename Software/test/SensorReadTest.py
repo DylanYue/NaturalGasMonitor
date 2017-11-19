1c0523996c2b49ba69aaa1bf8ce6503baccbef47
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-19 16:33:40 -0600

    New edits

--- Software/test/SensorReadTest.py
+++ Software/test/SensorReadTest.py
@@ -1,18 +1,15 @@
+""" This code runs the data acquisition and write to a file endlessly.
+	Author: Dingjun Yue
+	Date: Nov/2017
+"""
 
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
 import time
-
-# Import SPI library (for hardware SPI) and MCP3008 library.
-import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 
 # Software SPI configuration:
 CLK  = 18
-MISO = 23
+MISO = 16
 MOSI = 24
 CS   = 25
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
