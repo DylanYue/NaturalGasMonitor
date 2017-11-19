fbf6b170a9bd478b1e927bf67aff2461731ce81d
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-19 15:40:48 -0600

    Let's try again

--- Software/test/SensorReadTest.py
+++ Software/test/SensorReadTest.py
@@ -30,6 +30,7 @@ print('-' * 57)
 # Main program loop.
 while True:
     # Read all the ADC channel values in a list.
+	values = [0]*8
 	for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
 		values[i] = mcp.read_adc(i)
