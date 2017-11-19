b30e2f7f3a6a98dee4fe38715e5270071fd6c7ba
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-19 22:32:20 +0000

    It's working

--- Software/test/SensorReadTest.py
+++ Software/test/SensorReadTest.py
@@ -12,7 +12,7 @@ import Adafruit_MCP3008
 
 # Software SPI configuration:
 CLK  = 18
-MISO = 23
+MISO = 16
 MOSI = 24
 CS   = 25
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
