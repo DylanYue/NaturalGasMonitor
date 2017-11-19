247934117efe0425a067099ff36df2bf0ce669fb
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-19 17:07:11 -0600

    Sensor test code

--- Software/test/SensorReadTest.py
+++ Software/test/SensorReadTest.py
@@ -34,7 +34,7 @@ while True:
 	rawValue = mcp.read_adc(0)
 	floatRaw = float(rawValue)
 	rawVolt = floatRaw * refVolt / 1024.0
-	pressureMPa = (rawVolt - 0.5) / 4.0
+	pressureMPa = (rawVolt) / 4.0
 	pressureKPa = pressureMPa * 1000.0
 	
 	valueString = str(datetime.now().time()) + "," + str(pressureKPa) + '\n'
