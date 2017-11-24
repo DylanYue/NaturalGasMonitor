02640011957d624353abb23459c703ca396a4cf3
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-23 18:30:13 -0600

    Added os function

--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -302,7 +302,7 @@ class TimeState(State):
 class SDCardState(State):
 	
 	def __init__(self):
-		self.total_capacity = check_output('df -h | awk 'NR==2{print $2; exit}'').strip()
+		self.total_capacity = check_output('df -h | awk \'NR==2 {print $2; exit}\'').strip()
 		ClearTextArea()
 		DrawText(14, 0, self.total_capacity)
 
