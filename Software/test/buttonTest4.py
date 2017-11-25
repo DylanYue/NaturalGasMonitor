c679ce1a3913e5cf99333b8a619d73cdc113a43b
Dingjun Yue <dingjun.yue@gmail.com>
2017-11-24 22:37:40 -0600

    Combined push button class into one file

--- Software/test/buttonTest4.py
+++ Software/test/buttonTest4.py
@@ -6,6 +6,7 @@
 import os
 import time
 from datetime import datetime
+import RPi.GPIO as GPIO
 
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
@@ -15,7 +16,6 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-from PushButton import PushButton
 
 #from subprocess import check_output
 
@@ -355,6 +355,49 @@ class Selector(object):
 	# Return the current position of the selector on the screen (0 - 5)
 		return self.selectorPos % 6
 #----------------------------End Selector Class-------------------------------------#
+
+#----------------------------Push Button Class--------------------------------------#
+class PushButton(object):
+
+    def __init__(self, pin_number):
+        self.pin_number = pin_number
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin_number, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+    def ButtonDown(self):
+        """ This method detects if the button is down
+            True  ==> button down
+            False ==> button up
+        """
+        return True if not GPIO.input(self.pin_number) else False
+
+    def ButtonPressed(self):
+        """ This method detects if the button is pressed once
+            True   ==> button pressed once
+            False  ==> button not pressed
+        """
+        pressed = not GPIO.input(self.pin_number)
+        time.sleep(0.02)  # Wait 0.02s for the bouncing effect of physical switches
+        return True if pressed else False
+
+    def HoldTime(self):
+        """ This method returns the time the button is held down
+        """
+        # Return 0 is button is not down.
+        if not self.ButtonDown():
+            return 0
+        # Return the time the button is held down.
+        else:
+            start = time.time()
+            while self.ButtonDown():
+                time.sleep(0.01)
+                # If the button has been holding down for more than 5 seconds
+                # break the loop and return the hold time.
+                if (time.time() - start) > 5:
+                    break
+            length = time.time() - start
+            return length
+#---------------------------End Push Button Class-------------------------------------#			
 
 #----------------------------File IO Functions--------------------------------------#
 def InitializeFile():
